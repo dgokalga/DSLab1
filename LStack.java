d74b9469a257e1154a879893b5483c6feb4d6f0c
dgokalga <32280194+dgokalga@users.noreply.github.com>
2018-07-03 14:20:58 -0700

    Update LStack.java

--- LStack.java
+++ LStack.java
@@ -1,86 +1,104 @@
-/*Lab 1: Linked List String Stack Class 
+/* Lab 1
  *
- *Author: Devesh Gokalgandhi
+ * Module: LStack.java
  *
- *Methods: is_Empty, push, pop, peek, emptyLStack 
+ * Author: D.G.
+ *
+ * The purpose of this module is to create a linked-stack implementation.
+ *  
+ * Methods: is_Empty(), peek(), push(), pop(), emptyLStack(), getSize()
  *
  */
 
 
 class Node {
-	public char data; // Stack items will be of String type
-	public Node next; // 
+	public char data; // stack items will be of char datatype
+	public Node next; // points to data in transition
 }
 
-public class LStack {  
+public class LStack {
 	private Node top; // Node type pointing to the top of the stack
 	private int size;
+
 	/*
-	 * Constructor method
-	 * instantiates top node
+	 * Constructor method instantiates top node
 	 */
 	public LStack() {
 		top = new Node();
 		top.next = null; // empty Stack with top being null
 		size = 0;
-	} 
-	
+	}
+
 	/*
-	 * Verifies whether stack is empty or not 
+	 * Verifies whether stack is empty or not
 	 * 
 	 * returns boolean type True (if empty) or False (not empty)
 	 */
 	public boolean is_Empty() {
 		if (top.next == null) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	/*
+	 * Observes the top element of the stack 
 	 * 
-	 * 
+	 * Returns the top element of the stack
 	 */
 	public char peek() {
 		return top.data;
 	}
+	
+	/*
+	 * Adds element to the top of the stack 
+	 * Input: char datatype element
+	 */
 	public void push(char item) {
 		Node temp = new Node();
 		temp.data = item;
 		temp.next = top;
 		top = temp;
 		size++;
-		}
-	
+	}
+
+	/*
+	 * Removes top element of the stack
+	 * 
+	 * Returns removed element 
+	 */
 	public char pop() {
 		char item;
 		Node temp = top;
 		if (is_Empty() == true) {
 			return top.data;
-		}
-		else {
+		} else {
 			item = top.data;
 			top = top.next;
 			temp.next = null;
 			size--;
 			return item;
-			
+
 		}
 	}
 	
+	/*
+	 * Removes every element in stack
+	 */
 	public void emptyLStack() {
-		while(is_Empty() == false) {
+		while (is_Empty() == false) {
 			pop();
 		}
 	}
+
+	/*
+	 * Determines size of the stack
+	 * 
+	 * Returns int size
+	 */
 	public int getSize() {
 		return size;
 	}
 
-	
 }
-
-
-
